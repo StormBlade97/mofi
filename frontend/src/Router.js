d388e4eccc58692dc849d425623f7330d35ae115
Sebastian Wilzbach <seb@wilzba.ch>
2017-11-24 21:05:42 +0100

    #8 - switch to swing library for card swiping

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -5,14 +5,14 @@ import {
 } from 'react-router-dom'
 
 import StartPage from './views/StartPage';
-import SelectionPage from './views/SelectionPage';
+import SelectionPageSwing from './swing/SelectionPageSwing';
 import ResultPage from './views/ResultPage';
 
 export default () => (
   <Router>
     <div>
       <Route exact path="/" component={StartPage}/>
-      <Route path="/selection" component={SelectionPage}/>
+      <Route path="/selection" component={SelectionPageSwing}/>
       <Route path="/result" component={ResultPage}/>
     </div>
   </Router>

b5e661a90a89e82b06ba857819ba1766a668c7fc
Christian Tschoepe <tschoepe.christian@gmail.com>
2017-11-25 23:59:18 +0200

    Fixed the movies a user gets and the order they get them in.

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,6 +1,3 @@
-import Mood from './mood'
-import MovieMood from './movie_mood'
 import MovieSession from './movie_session'
-import Movie from './movie'
 
-export { Mood, MovieMood, MovieSession, Movie }
+export { MovieSession }
--- backend/src/models/movie.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema, SchemaTypes } from 'mongoose';
-
-const schema = new Schema({
-    id: String
-});
-
-const Movie = mongoose.model('Movie', schema);
-
-export default Movie;

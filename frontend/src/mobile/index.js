4cb425991396e13bbf2face597fc3632ad9e3504
Sebastian Wilzbach <seb@wilzba.ch>
2017-11-26 07:33:16 +0200

    Add simple page transition

--- frontend/src/mobile/index.js
+++ frontend/src/mobile/index.js
@@ -12,6 +12,7 @@ import {initUserStore} from './UserStore';
 import ResultPage from './views/ResultPage';
 import FilterPage from './filterview';
 import Logo from '../elisa_logo.png'
+import { AnimatedSwitch } from 'react-router-transition';
 
 const ElisaImg = styled.img`
   height: 5vh;
@@ -31,11 +32,18 @@ export default class IndexView extends Component {
     return (
       <div>
         <ElisaImg alt="Elisa" src={Logo} />
+    <AnimatedSwitch
+      atEnter={{ opacity: 0 }}
+      atLeave={{ opacity: 0 }}
+      atActive={{ opacity: 1 }}
+      className="switch-wrapper"
+    >
         <Route exact path={`${match.url}`} component={LandingPage}/>
         <Route path={`${match.url}/c/:code`} component={LandingPage}/>
         <Route exact path={`${match.url}/mood-filter`} component={FilterPage}></Route>
         <Route path={`${match.url}/selection`} component={SelectionPageSwing}/>
         <Route path={`${match.url}/result`} component={ResultPage}/>
+   </AnimatedSwitch>
       </div>
     )
   }
